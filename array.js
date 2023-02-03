b795591af00641d88b197b922691c4701df95d50
sajedulislamsaju <sajedulislamsaju123@gmail.com>
2023-02-03 12:04:12 +0600

    first commit

---
+++ array.js
@@ -0,0 +1,28 @@
+const names = 'bangladesh';
+const age = 54;
+const Independence = true;
+const friend ={id:3760, age:34};
+const ages =[12, 13, 14, 15 ,23 ,45,];
+function add(num1 , num2 ){
+        return num1 + num2
+}
+console.log(typeof names)
+console.log(typeof age)
+console.log(typeof Independence)
+console.log(typeof friend)
+// console.log(typeof ages)
+console.log(typeof add)
+console.log(Array.isArray(ages))
+
+console.log(ages.includes(45))
+
+
+const moreFriend = [48,56,66,54,33,53,35,34,]
+
+const allfroend = moreFriend.concat(ages)
+
+console.log(allfroend)
+
+console.log(moreFriend + ages)
+
+
